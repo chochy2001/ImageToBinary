2ee859791c70750d64db10496c5c23983b2da066
chochy2001 <ohchochy@gmail.com>
2024-05-05 17:02:29 -0600

    image conversion to binary

---
+++ image_binary.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	_ "image/png"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+func main() {
+	// Abre el archivo de imagen
+	file, err := os.Open("./images/imagen3.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Decodifica la imagen
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// Redimensiona la imagen a 640x480
+	img = resize.Resize(640, 480, img, resize.Lanczos3)
+
+	// Crear archivo de salida
+	outFile, err := os.Create("imagen3.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+
+	// Convierte la imagen a binario con umbral
+	threshold := 50
+	for y := 0; y < img.Bounds().Dy(); y++ {
+		outFile.WriteString("\"")
+		for x := 0; x < img.Bounds().Dx(); x++ {
+			// Obtiene el valor del pixel en escala de grises
+			grayColor := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+			if grayColor.Y > uint8(threshold) {
+				outFile.WriteString("0")
+			} else {
+				outFile.WriteString("1")
+			}
+		}
+		outFile.WriteString("\",\n")
+	}
+}
